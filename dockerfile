fcfda86a20eb1339ccedf24c75ee84fa7ee2a061
Prabal Pratap Singh <ppsingh.singh2@gmail.com>
2023-10-24 17:49:04 +0530

    run local kube success

--- dockerfile
+++ dockerfile
@@ -1,3 +1,3 @@
-FROM openjdk:8-jdk-alpine
+FROM openjdk:11
 COPY target/gcp-demo-project-0.0.1-SNAPSHOT.jar gcp-demo-project-0.0.1.jar
 ENTRYPOINT ["java","-jar","/gcp-demo-project-0.0.1.jar"]
--- src/main/java/com/cloud/gcp/demo/gcpdemoproject/service/UserServiceImpl.java
+++ src/main/java/com/cloud/gcp/demo/gcpdemoproject/service/UserServiceImpl.java
@@ -1,11 +1,12 @@
 package com.cloud.gcp.demo.gcpdemoproject.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
 import com.cloud.gcp.demo.gcpdemoproject.domain.pojo.User;
-import com.google.common.collect.ImmutableList;
+
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -13,12 +14,13 @@ public class UserServiceImpl implements UserService {
 	@Override
 	public List<User> findAllUsers() {
 		
-		List<User> users = ImmutableList.of( 
-				new User(1,"Rohit", 36, "Mumbai"),
-				new User(2,"Virat", 35, "Delhi"),
-				new User(3,"Gill", 36, "Punjab"),
-				new User(4,"KL Rahul", 36, "Karnatka"),
-				new User(5,"Shreyas", 36, "Mumbai"));
+		List<User> users = new ArrayList<>();
+		users.add(new User(1,"Rohit", 36, "Mumbai")); 
+				
+				users.add(new User(2,"Virat", 35, "Delhi"));
+						users.add(new User(3,"Gill", 36, "Punjab"));
+								users.add(new User(4,"KL Rahul", 36, "Karnatka"));
+										users.add(new User(5,"Shreyas", 36, "Mumbai"));
 		return users;
 	}
 
